b568d39bddd4c0d2f1f916f653097ba5d234b8b5
Lex <pineapplepear55@gmail.com>
2023-07-28 21:12:43 +0300

    feat: port storing to env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TrackModule } from './routes/track/track.module';
 import { ArtistModule } from 'src/routes/artist/artist.module';
 import { AlbumModule } from 'src/routes/album/album.module';
 import { FavoritesModule } from 'src/routes/favs/favs.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -14,6 +15,10 @@ import { FavoritesModule } from 'src/routes/favs/favs.module';
     ArtistModule,
     AlbumModule,
     FavoritesModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 import * as yaml from 'js-yaml';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const file = fs.readFileSync(path.join(__dirname, '../doc/api.yaml'), 'utf8');
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
 
   const docFile = yaml.load(file);
   SwaggerModule.setup('doc', app, docFile as OpenAPIObject);
-  await app.listen(4000);
+  await app.listen(port || 4000);
 }
 bootstrap();
